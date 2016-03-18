370103d929dcb6acd1395f312bfc4954d0f4bdf4
Arslan Zagdiullin <arslan.dev@gmail.com>
2016-03-18 23:46:35 +0300

    Initial Commit

---
+++ TestAppDelTests/TestAppDelTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestAppDelTests.swift
+//  TestAppDelTests
+//
+//  Created by Arslan Zagidullin on 18/03/16.
+//  Copyright © 2016 Arslan Zagidullin. All rights reserved.
+//
+
+import XCTest
+@testable import TestAppDel
+
+class TestAppDelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
